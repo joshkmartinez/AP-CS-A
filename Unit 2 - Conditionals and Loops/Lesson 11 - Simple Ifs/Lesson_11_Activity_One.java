5426d1e14510a425d2b125aad569e03de7111957
Joshua K. Martinez <joshkmartinez@gmail.com>
2018-11-09 16:03:39 -0800

    added some code formatting

--- Unit 2 - Conditionals and Loops/Lesson 11 - Simple Ifs/Lesson_11_Activity_One.java
+++ Unit 2 - Conditionals and Loops/Lesson 11 - Simple Ifs/Lesson_11_Activity_One.java
@@ -4,19 +4,16 @@ import static java.lang.System.*;
 
 import java.util.Scanner;
 
-class Lesson_11_Activity_One{
+class Lesson_11_Activity_One {
 
+  public static void main(String str[]) throws IOException {
 
-     public static void main (String str[]) throws IOException {
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    if (num == 176) {
+      System.out.println("YES");
+    }
 
-       Scanner scan = new Scanner (System.in);
-       int num = scan.nextInt();
-       if (num ==176){
-       System.out.println("YES");
-       }
-
-     }
+  }
 
 }
-
-
